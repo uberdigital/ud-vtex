f137ff81ba649e3bab8774613c2a93947c97c637
Felipe Gomes <felipegomes@uberdigital.com.br>
2013-02-28 17:56:08 -0300

    Alterações de checkout

--- denko/arquivos/user.js
+++ denko/arquivos/user.js
@@ -1,17 +1,7 @@
 $(document).ready(function() {
 
-WebFontConfig = {
-    google: { families: [ 'Share+Tech+Mono::latin' ] }
-  };
-  (function() {
-    var wf = document.createElement('script');
-    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-    wf.type = 'text/javascript';
-    wf.async = 'true';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(wf, s);
-  })();
+
+  //$('body').appendTo('<script type="text/javascript">WebFontConfig={google:{families:["Share+Tech+Mono::latin"]}};(function(){var a=document.createElement("script");a.src=("https:"==document.location.protocol?"https":"http")+"://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js";a.type="text/javascript";a.async="true";var b=document.getElementsByTagName("script")[0];b.parentNode.insertBefore(a,b)})();</script>');
 
   $("body.checkout .searchWrapper:last, body.minha-conta .searchWrapper:last, body.fale-conosco .searchWrapper:last, body.sku .searchWrapper:last").addClass('bottomSearchWrapper');
 
